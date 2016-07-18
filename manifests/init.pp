134a15fa85e29aa042b8fb379de29e298ef2da11
Alex Dreyer <alex@puppetlabs.com>
2016-07-18 10:25:00 -0700

    (maint) fix lint errors

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,15 +10,15 @@ application wordpress_app (
   Array  $lb_options      = ['forwardfor','http-server-close','httplog'],
 ){
   $db_components = collect_component_titles($nodes, Wordpress_app::Database)
-  if (size($db_components) != 1){
+  if (size($db_components) != 1) {
     $db_size = size($db_components)
     fail("There must be one database component for wordpress_app. found: ${db_size}")
   }
   wordpress_app::database { $db_components[0]:
-    database   => $database,
-    user       => $db_user,
-    password   => $db_pass,
-    export     => Database["wdp-${name}"]
+    database => $database,
+    user     => $db_user,
+    password => $db_pass,
+    export   => Database["wdp-${name}"]
   }
 
   # Collect the titles of all Web components declared in nodes.
@@ -31,7 +31,7 @@ application wordpress_app (
   # Http resources from them for the load balancer.
   $web_https = $web_components.map |$comp_name| {
     # Compute the Http resource title for export and return.
-    $http = Http["web-$comp_name"]
+    $http = Http["web-${comp_name}"]
     # Declare the web component.
     wordpress_app::web { $comp_name:
       apache_port => $web_port,
